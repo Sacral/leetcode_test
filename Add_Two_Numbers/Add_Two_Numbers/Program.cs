8bd08de6645ae2757b937ca6e643b2ae66e1de44
lisay820917 <lisay820917@gmail.com>
2021-01-27 11:28:56 +0800

    leetcode add two numbers

---
+++ Add_Two_Numbers/Add_Two_Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l = new ListNode();
+
+            ListNode l3 = new ListNode {
+
+                val = { 5, 6, 4 }
+
+            };
+
+            ListNode l4 = new ListNode { 5, 6, 4};
+
+            AddTwoNumbers(l3,l4);
+
+
+        }
+
+        
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+                {
+                    this.val = val;
+                    this.next = next;
+                }
+          }
+
+
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+
+
+
+
+
+            return 0 ;
+        }
+
+
+
+    }
+}
