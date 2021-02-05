68049d0f9a7d60a74c555e0b4c9d648754eaefa2
lisay820917 <lisay820917@gmail.com>
2021-02-05 12:00:51 +0800

    Search in Rotated Sorted Array Runtime: 92 ms, faster than 76.62% of C# online submissions for Search in Rotated Sorted Array. Memory Usage: 25.4 MB, less than 51.60% of C# online submissions for Search in Rotated Sorted

---
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Search_in_Rotated_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] nums = {4, 5, 6, 7, 0, 1, 2};
+
+
+
+            Console.WriteLine(Search(nums,0));
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+
+            List<int> list_num = new List<int>();
+
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                list_num.Add(nums[i]);
+
+            }
+
+            int num_index = list_num.IndexOf(target);
+
+
+
+
+            return num_index;
+        }
+
+
+    }
+}
