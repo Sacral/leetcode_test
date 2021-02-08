383d41eaf6a7ca80c2cd3d13f4bf0e2762f11722
lisay820917 <lisay820917@gmail.com>
2021-02-08 17:10:30 +0800

    Search a 2D Matrix Runtime: 100 ms, faster than 44.69% of C# online submissions for Search a 2D Matrix. Memory Usage: 25.3 MB, less than 68.36% of C# online submissions for Search a 2D Matrix.

---
+++ Search a 2D Matrix/Search a 2D Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Search_a_2D_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int[][] matrix;
+
+            //matrix = new int[][] { new int[] { 1, 3, 5, 7 }, new int[] { 10, 11, 16, 20 }, new int[] { 23, 30, 34, 60 } };
+            matrix = new int[][] { new int[] { -1, 3 } };
+            //matrix = new int[][] { new int[] { 1 } , new int[] { 3 } };
+
+            // SearchMatrix(matrix,3);
+            Console.WriteLine(SearchMatrix(matrix, 3));
+
+        }
+
+
+
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+
+            for (int i=0;i<matrix.GetLength(0);i++)
+            {
+
+                if (matrix[i][0] <= target || matrix[i][matrix[i].Length-1] >= target)
+                {
+
+                    for(int j = 0; j < matrix[i].Length; j++)
+                    {
+                        if (matrix[i][j] == target)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
